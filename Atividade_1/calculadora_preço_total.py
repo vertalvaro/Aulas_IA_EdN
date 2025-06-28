9de69410ff624e60aa4727da16787110593dafc6
Alvaro Miguel <alvarmiguelmart@gmail.com>
2025-06-28 16:33:15 -0300

    Atividade 1 que faltava

---
+++ Atividade_1/calculadora_preço_total.py
@@ -0,0 +1,10 @@
+produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", produto)
+print("Preço unitário: R$", round(preco_unitario, 2))
+print("Quantidade:", quantidade)
+print("Preço total: R$", round(preco_total, 2))
